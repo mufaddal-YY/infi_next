4d7cfb3b32365b46af557d41d2702acd6a0e9833
mufaddal <mufaddalaurangabadi@gmail.com>
2023-09-07 17:39:06 +0530

    fixes recent

--- next.config.js
+++ next.config.js
@@ -1,31 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-          port: "",
-        },
-      ],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-      return config;
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  };
+  experimental: {
+    appDir: true,
+  },
+  distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
